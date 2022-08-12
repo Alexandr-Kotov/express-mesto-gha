a531fa90f787e8df125a705d2dccc9366d4974c1
Alexandr Kotov <Mr1Hiroshi@yandex.ru>
2022-08-12 22:39:58 +0500

    test

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,13 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUser = (req, res) => {
   User.findId(req.params.id)
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Произошла ошибка при запросе пользователя' });
+      } else {
+        res.send({ data: user });
+      }
+    })
     .catch(() => res.status(500).send({ message: 'Произошла ошибка при запросе пользователя' }));
 };
 
